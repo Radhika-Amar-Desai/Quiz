581b77e20d95b67771fba41912d1637f4a95bb09
Aadi Umrani <aadi.p.um@gmail.com>
2024-06-21 16:08:29 -0400

    Revamped the UI

--- src/Question.js
+++ src/Question.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { ButtonGroup, Container } from '@mui/material';
 
 const Question = ({ question, answerOptions, onSelectOption }) => {
   return (
-    <div>
-      <p>{question}</p>
-      {answerOptions.map((option, index) => (
-        <button key={index} onClick={() => onSelectOption(index)}>
-          {option}
-        </button>
-      ))}
-    </div>
+    <Container style={{
+      padding: '30px 50px',
+      width: 'fit-content',
+      borderRadius: '10px',
+      boxShadow: '5px 5px 10px #999999',
+    }}>
+      <Typography variant="h5" style={{
+        marginBottom: '20px',
+        textAlign: 'center',
+      }}>{question}</Typography>
+      <ButtonGroup variant="outlined" disableElevation aria-label="Basic button group">
+        {answerOptions.map((option, index) => (
+          <Button disableElevation
+            variant="contained" 
+            color="primary" 
+            key={index} 
+            onClick={() => onSelectOption(index)}
+          >
+            {option}
+          </Button>
+        ))}
+      </ButtonGroup>
+    </Container>
   );
 };
 
